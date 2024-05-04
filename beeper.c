f7e5c4626e015f6f0dd3bc80c91337297337f160
Meelis Utt <meelis.utt@gmail.com>
2024-05-04 17:34:30 +0200

    protocol marshalling and unmarshalling

--- beeper.c
+++ beeper.c
@@ -4,14 +4,25 @@
 #include "protocol.h"
 
 int main() {
-	Packet packet = {.version_minor=1, .version_major=0, .repeat=0, .timer=4.76, .msg="hey"};
+	Packet packet = {.version_minor='1', .version_major='0', .repeat=1, .timer=4.76, .msg=(unsigned char*)"hey"};
+	print_packet(packet);
 	uint8_t *buf = marshal(packet);
-	printf("%s\n", buf+5);
+	if (buf == NULL) {
+		fprintf(stderr, "invalid version: got %d.%d, expected %d.%d\n", packet.version_major, packet.version_minor, VERSION_MAJOR, VERSION_MINOR);
+		return 1;
+	}
+	printf(".msg=%s\n", buf+5);
 	for (int i=0;i<METADATA_SIZE;++i) {
 		printf("%u\n", buf[i]);
 	}
 
-	print_packet(packet);
+	Packet *pckt = unmarshal(buf);
+	if (pckt == NULL) {
+		fprintf(stderr, "failed to unmarshal buf\n");
+		return 1;
+	}
+	print_packet(*pckt);
 
 	free(buf);
+	free_packet(pckt);
 }
--- protocol.h
+++ protocol.h
@@ -6,12 +6,21 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <math.h>
 
 void mmcp(uint8_t *dest, const uint8_t *src, uint8_t len) {
-	uint8_t *src_cp = (uint8_t *)src;
-	for (uint8_t i=0; i<len || (*src_cp) == '\0'; ++i, ++src_cp) {
+	unsigned char *src_cp = (unsigned char*)src;
+	for (uint8_t i=0; i<len || (*src_cp) != '\0'; ++i, ++src_cp) {
 		dest[i] = src[i];
 	}
+}
+
+uint32_t sz(uint8_t *buf) {
+	uint32_t size = 0;
+	for (; *(buf+size) != '\0' && size<UINT8_MAX; ++size) {
+		;
+	}
+	return size;
 }
 
 enum METADATA_IDX {
@@ -23,8 +32,8 @@ enum METADATA_IDX {
 	METADATA_SIZE,
 };
 
-#define VERSION_MAJOR (0)
-#define VERSION_MINOR (1)
+#define VERSION_MAJOR ('0')
+#define VERSION_MINOR ('1')
 #define PACKET_SIZE (UINT8_MAX)
 #define MSG_SIZE (PACKET_SIZE-METADATA_SIZE)
 
@@ -33,29 +42,84 @@ typedef struct {
 	uint8_t version_major;
 	bool repeat;
 	double timer;
-	unsigned char msg[MSG_SIZE+1];
+	unsigned char *msg; //[MSG_SIZE+1];
 } Packet;
 
 void print_packet(Packet packet);
+void free_packet(Packet *packet);
 uint8_t *marshal(Packet packet); // allocs memory
-Packet unmarshal(uint8_t *buf); // allocs memory
+Packet *unmarshal(uint8_t *buf); // allocs memory
+void send(); // TODO:
+void receive(); // TODO:
 
 void print_packet(Packet packet) {
-	printf("{\n\t.version_minor=%u,\n\t.version_major=%u,\n\t.repeat=%u\n\t.timer=%0.2f\n\t.msg=%s\n}\n", packet.version_minor, packet.version_major, packet.repeat, packet.timer, packet.msg);
+	printf("Packet = {"
+		"\n\t.version_minor=%c"
+		"\n\t.version_major=%c"
+		"\n\t.repeat=%u"
+		"\n\t.timer=%0.1f"
+		"\n\t.msg=%s"
+		"\n}\n",
+		packet.version_minor,
+		packet.version_major,
+		packet.repeat,
+		floor(packet.timer*10.0)/10.0,
+		packet.msg
+	);
 }
 
+void free_packet(Packet *packet) {
+	if (packet == NULL) {
+		return;
+	}
+	if (packet->msg != NULL) {
+		free(packet->msg);
+	}
+	free(packet);
+}
+
+
 uint8_t *marshal(Packet packet) {
-	uint8_t *buf = calloc(PACKET_SIZE, sizeof(uint8_t));
-	buf[0] = packet.version_minor;
-	buf[1] = packet.version_major;
-	buf[2] = (uint8_t)packet.repeat;
-	buf[3] = (uint8_t)((uint32_t)(packet.timer*10.0)%10);
-	buf[4] = (uint8_t)packet.timer;
-	mmcp(buf+(uint8_t)5, packet.msg, MSG_SIZE);
+	if (packet.version_major != VERSION_MAJOR) {
+		fprintf(stderr, "[ERROR]: [MVMAMM]: marshal major version mismatch: expected %c, got %c\n", VERSION_MAJOR, packet.version_major);
+		return NULL;
+	}
+	if (packet.version_minor != VERSION_MINOR) {
+		fprintf(stderr, "[ERROR]: [MVMIMM]: marshal minor version mismatch: expected %c, got %c\n", VERSION_MINOR, packet.version_minor);
+		return NULL;
+	}
+	uint8_t *buf = calloc(PACKET_SIZE+1, sizeof(uint8_t));
+	buf[IDX_VERSION_MINOR] = packet.version_minor;
+	buf[IDX_VERSION_MAJOR] = packet.version_major;
+	buf[IDX_REPEAT] = (uint8_t)packet.repeat+'0';
+	buf[IDX_TIMER_FRAC] = (uint8_t)((uint32_t)(packet.timer*10.0)%10);
+	buf[IDX_TIMER_INT] = (uint8_t)packet.timer;
+	mmcp(buf+(uint8_t)METADATA_SIZE, packet.msg, MSG_SIZE);
 	return buf;
 }
 
-Packet unmarshal(uint8_t *buf) {
+Packet *unmarshal(uint8_t *buf) {
+	uint32_t buf_size = sz(buf);
+	if (buf_size < METADATA_SIZE) {
+		fprintf(stderr, "[ERROR]: [UMBSLTMD]: unmarshal buffer size less than metadata: expected >%d, got %d\n", METADATA_SIZE, buf_size);
+		return NULL;
+	}
+	if (buf[IDX_VERSION_MAJOR] != VERSION_MAJOR) {
+		fprintf(stderr, "[ERROR]: [UMVMAMM]: unmarshal major version mismatch: expected %c, got %c\n", VERSION_MAJOR, buf[IDX_VERSION_MAJOR]);
+		return NULL;
+	}
+	if (buf[IDX_VERSION_MINOR] != VERSION_MINOR) {
+		fprintf(stderr, "[ERROR]: [UMVMIMM]: unmarshal minor version mismatch: expected %c, got %c\n", VERSION_MINOR, buf[IDX_VERSION_MINOR]);
+		return NULL;
+	}
+	Packet *packet = calloc(1, sizeof(Packet));
+	packet->version_minor = buf[IDX_VERSION_MINOR];
+	packet->version_major = buf[IDX_VERSION_MAJOR];
+	packet->repeat = buf[IDX_REPEAT]-'0';
+	packet->timer = buf[IDX_TIMER_INT]+(double)buf[IDX_TIMER_FRAC]/10.0;
+	packet->msg = calloc(MSG_SIZE+1, sizeof(unsigned char));
+	mmcp(packet->msg, buf+METADATA_SIZE, MSG_SIZE);
+	return packet;	
 }
 
 #endif // PROTOCOL_H_
